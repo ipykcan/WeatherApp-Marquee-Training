77b0ac8611651ffb4de29d95ca707a16823e7bcc
poojasrib17 <developerpoojab@gmail.com>
2026-07-08 09:42:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let cityInput = document.getElementById("cityInput");
+let btn = document.getElementById("getWeatherBtn");
+let display = document.getElementById("weatherDisplay");
+let descriptionDisplay = document.getElementById("descriptionDisplay");
+let feelsLikeDisplay = document.getElementById("feelsLikeInfo");
+let humidityDisplay = document.getElementById("humidityInfo");
+let windDisplay = document.getElementById("windInfo");
+let seaLevelDisplay = document.getElementById("seaLevelInfo");  
+btn.addEventListener("click", () => {
+    let city = cityInput.value;
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=53cf92b73fb02d94ca60985477592309`;
+
+    console.log(city);
+    console.log(url);
+
+    async function getWeather() {
+let response = await fetch(url);
+let data = await response.json();
+
+const temp = Math.round(data.main.temp);
+console.log(temp);
+
+const description = data.weather[0].description;
+const feelsLike = Math.round(data.main.feels_like);
+const humidity = data.main.humidity;
+const windSpeed = data.wind.speed;
+const seaLevel = data.main.sea_level;
+console.log(description);
+console.log(feelsLike);
+console.log(humidity);
+console.log(windSpeed);
+console.log(seaLevel);
+    display.innerHTML = `${temp}°C`;
+    descriptionDisplay.innerHTML = `Details: ${description}`;
+    feelsLikeDisplay.innerHTML = `Feels like: ${feelsLike}°C`;
+    humidityDisplay.innerHTML = `Humidity: ${humidity}%`;
+    windDisplay.innerHTML = `Wind Speed: ${windSpeed} m/s`;
+    seaLevelDisplay.innerHTML = `Sea Level: ${seaLevel}`;
+}
+
+getWeather(); 
+
+});
+
+
+
+
+
